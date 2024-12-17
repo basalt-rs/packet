9a19822398c430973cda8a0e683194e9da0b0db3
JingusJohn <branchjohnchristopher@gmail.com>
2024-12-17 14:32:41 -0600

    added toml, added title, preamble

--- src/packet.rs
+++ src/packet.rs
@@ -4,14 +4,16 @@ use serde::{Deserialize, Serialize};
 
 /// Represents a Packet containing questions and
 /// configurations
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Packet {
+    title: Option<String>,
+    preamble: Option<String>,
     default_language: Option<String>,
-    languages: Option<String>,
+    languages: Option<Vec<String>>,
     problems: Vec<Problem>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Problem {
     default_language: Option<String>,
     languages: Option<String>,
@@ -19,7 +21,7 @@ pub struct Problem {
     tests: Vec<Test>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Test {
     input: String,
     output: String,
@@ -29,4 +31,14 @@ pub struct Test {
 
 fn default_visibility() -> bool {
     false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn packets_parse_correctly() {
+        // parse example one
+        let pkt: Packet = toml::from_str(include_str!("../examples/one.toml")).unwrap();
+    }
 }
