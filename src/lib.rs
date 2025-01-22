fc679ea0633328e47967f41affd4789dd0829fc3
jackjohn7 <branchjohnchristopher@gmail.com>
2025-01-21 18:05:23 -0600

    added from pathbuf

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! Contains tools related to packets.
 
+use std::{fs, path::PathBuf};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -48,12 +50,43 @@ fn default_false() -> bool {
     false
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum PacketReadError {
+    #[error("Failed to read packet file: {0}")]
+    FailedToReadPacketFile(std::io::Error),
+    #[error("Packet is malformed: {0}")]
+    MalformedPacketError(String),
+}
+
+impl TryFrom<PathBuf> for Packet {
+    type Error = PacketReadError;
+    fn try_from(value: PathBuf) -> Result<Self, Self::Error> {
+        // read content from file
+        let content_bytes =
+            fs::read(value).map_err(|e| PacketReadError::FailedToReadPacketFile(e))?;
+        let content = std::str::from_utf8(&content_bytes)
+            .map_err(|e| PacketReadError::MalformedPacketError(e.to_string()))?;
+        let packet: Packet = toml::from_str(content)
+            .map_err(|e| PacketReadError::MalformedPacketError(e.to_string()))?;
+        Ok(packet)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::{io::Write, path::PathBuf};
+    use tempfile::NamedTempFile;
+    const EXAMPLE_ONE_CONTENT: &str = include_str!("../examples/one.toml");
     #[test]
     fn packets_parse_correctly() {
         // parse example one
-        let _: Packet = toml::from_str(include_str!("../examples/one.toml")).unwrap();
+        let _: Packet = toml::from_str(EXAMPLE_ONE_CONTENT).unwrap();
+    }
+    #[test]
+    fn packet_files_parse_correctly() {
+        let mut file = NamedTempFile::new().unwrap();
+        file.write_all(EXAMPLE_ONE_CONTENT.as_bytes()).unwrap();
+        let _: Packet = Packet::try_from(PathBuf::from(file.path())).unwrap();
     }
 }
