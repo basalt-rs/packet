d6eb4347910d80bcc835bbf95430f1f7d80b19f6
funnyboy-roks <funnyboyroks@gmail.com>
2025-02-09 22:22:24 -0600

    fix broken html

--- examples/html.rs
+++ examples/html.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         r#"
 # hello world
 
-This is Euler's identity! blah $e pix i + 1 = 0$ blah
+This is Euler's identity! blah $e ^(pi i) + 1 = 0$ blah
 
 $$
 e ^(pi i) + 1 = 0
